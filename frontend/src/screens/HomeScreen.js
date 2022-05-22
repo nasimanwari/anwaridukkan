2b8d9134fbd43be0a67458c75e51d678b4e2ede4
nasim anwari <nasimanwari367@gmail.com>
2022-05-23 01:57:27 +0300

    Created product components

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,7 +1,10 @@
-import axios from 'axios';
-import { Link } from 'react-router-dom';
-import logger from 'use-reducer-logger';
 import { useEffect, useReducer, useState } from 'react';
+import axios from 'axios';
+import logger from 'use-reducer-logger';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Product from '../components/Product';
+// import data from '../data';
 
 const reducer = (state, action) => {
   switch (action.type) {
@@ -15,13 +18,13 @@ const reducer = (state, action) => {
       return state;
   }
 };
-
 function HomeScreen() {
   const [{ loading, error, products }, dispatch] = useReducer(logger(reducer), {
     products: [],
     loading: true,
     error: '',
   });
+  // const [products, setProducts] = useState([]);
   useEffect(() => {
     const fetchData = async () => {
       dispatch({ type: 'FETCH_REQUEST' });
@@ -31,37 +34,26 @@ function HomeScreen() {
       } catch (err) {
         dispatch({ type: 'FETCH_FAIL', payload: err.message });
       }
-
       // setProducts(result.data);
     };
     fetchData();
   }, []);
   return (
     <div>
-      <h1>Ürünler</h1>
+      <h1>Featured Products</h1>
       <div className="products">
         {loading ? (
           <div>Loading...</div>
         ) : error ? (
           <div>{error}</div>
         ) : (
-          products.map((product) => (
-            <div className="product" key={product.slug}>
-
-              <Link to={`/product/${product.slug}`}>
-                <img src={product.image} alt={product.name} />
-              </Link>
-              <div className="product-info">
-                <Link to={`/product/${product.slug}`}>
-                  <p>{product.name}</p>
-                </Link>
-                <p>
-                  <strong>${product.price}</strong>
-                </p>
-                <button>Add to cart</button>
-              </div>
-            </div>
-          ))
+          <Row>
+            {products.map((product) => (
+              <Col key={product.slug} sm={6} md={4} lg={3} className="mb-3">
+                <Product product={product}></Product>
+              </Col>
+            ))}
+          </Row>
         )}
       </div>
     </div>
