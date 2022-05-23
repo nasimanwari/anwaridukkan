2383a4f1ccb7b35dfe1c4d7be93a5dba7f696870
nasim anwari <nasimanwari367@gmail.com>
2022-05-24 01:41:52 +0300

    Connected to MongoDB database

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,37 +1,66 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
-import Container from 'react-bootstrap/Container';
-import {LinkContainer} from 'react-router-bootstrap';
+import CartScreen from './screens/CartScreen';
+import SigninScreen from './screens/SigninScreen';
 import Navbar from 'react-bootstrap/Navbar';
+import Badge from 'react-bootstrap/Badge';
+import Nav from 'react-bootstrap/Nav';
+import Container from 'react-bootstrap/Container';
+import { LinkContainer } from 'react-router-bootstrap';
+import { useContext } from 'react';
+import { Store } from './Store';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 
 function App() {
+  const { state } = useContext(Store);
+  const { cart } = state;
+  const { state, dispatch: ctxDispatch } = useContext(Store);
+  const { cart, userInfo } = state;
+
+  const signoutHandler = () => {
+    ctxDispatch({ type: 'USER_SIGNOUT' });
+    localStorage.removeItem('userInfo');
+  };
   return (
     <BrowserRouter>
-    <div className="d-flex flex-column site-container">
-      <header>
-        <Navbar bg="dark" variant="dark">
-          <Container>
-            <LinkContainer to="/">
-            <Navbar.Brand>anwari</Navbar.Brand>
-            </LinkContainer>
+      <div className="d-flex flex-column site-container">
+        <header>
+          <Navbar bg="dark" variant="dark">
+            <Container>
+              <LinkContainer to="/">
+                <Navbar.Brand>anwari</Navbar.Brand>
+              </LinkContainer>
+              <Nav className="me-auto">
+                <Link to="/cart" className="nav-link">
+                  Sepet
+                  {cart.cartItems.length > 0 && (
+                    <Badge pill bg="danger">
+                      {cart.cartItems.reduce((a, c) => a + c.quantity, 0)}
+                    </Badge>
+                  )}
+                </Link>
+              </Nav>
+            </Container>
+          </Navbar>
+        </header>
+        <main>
+          <Container className="mt-3">
+            <Routes>
+              <Route path="/product/:slug" element={<ProductScreen />} />
+              <Route path="/cart" element={<CartScreen />} />
+              <Route path="/signin" element={<SigninScreen />} />
+              <Route path="/" element={<HomeScreen />} />
+            </Routes>
           </Container>
-        </Navbar>
-      </header>
-      <main>
-      <Container className="mt-3">
-        <Routes>
-          <Route path="/product/:slug" element={<ProductScreen/>} />
-          <Route path="/" element={<HomeScreen/>} exact />
-        </Routes>
-        </Container>
-      </main>
-      <footer>
-        <div className="text-center">All rights reserved</div>
-      </footer>
-    </div>
-  </BrowserRouter>
+        </main>
+        <footer>
+          <div className="text-center">Tüm Haklı Saklıdır</div>
+        </footer>
+      </div>
+    </BrowserRouter>
   );
 }
-
 export default App;
